5fb084fcc61e34064de0a003297f11142c3d680d
davinawuy <alano.davin@office.ui.ac.id>
2024-09-11 11:26:14 +0700

    finished tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #Adding the templates directory to the TEMPLATES setting
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://alano-davin:a2A7HLCjWWcNGaxqpnvojWWxAf2L5b0C@pbp.cs.ui.ac.id/alano-davin/mentalhealthtrackerr", "https://alano-davin:a2A7HLCjWWcNGaxqpnvojWWxAf2L5b0C@pbp.cs.ui.ac.id/alano-davin/mentalhealthtrackerr"]
