aa50d663ecff427b877315f2bf48b19dbecd5617
davinawuy <alano.davin@office.ui.ac.id>
2024-11-13 11:54:07 +0700

    finishing Tutorial 8

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.shortcuts import render, redirect, reverse   # Import redirect and render
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
-
+import json
 
 
 
@@ -134,3 +134,21 @@ def delete_mood(request, id):
     mood.delete()
     # Return to home page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+def create_mood_flutter(request):
+        if request.method == 'POST':
+
+            data = json.loads(request.body)
+            new_mood = MoodEntry.objects.create(
+                user=request.user,
+                mood=data["mood"],
+                mood_intensity=int(data["mood_intensity"]),
+                feelings=data["feelings"]
+            )
+
+            new_mood.save()
+
+            return JsonResponse({"status": "success"}, status=200)
+        else:
+            return JsonResponse({"status": "error"}, status=401)
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -30,6 +30,12 @@ DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alano-davin-mentalhealthtrackerr.pbp.cs.ui.ac.id"]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 # Application definition
 
@@ -40,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', # Add it directly under SecurityMiddleware
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
